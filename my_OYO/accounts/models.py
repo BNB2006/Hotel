65761b4dfadf9e8a5b9dc0f798a9039e78292830
Balaji Bandgar <balajibandgar26@gmail.com>
2025-07-08 18:04:19 +0530

    Amenities

--- my_OYO/accounts/models.py
+++ my_OYO/accounts/models.py
@@ -26,7 +26,7 @@ class HotelVendor(User):
 
 class Ameneties(models.Model):
     name = models.CharField(max_length = 1000)
-    icon = models.ImageField(upload_to="hotels")
+    icon = models.ImageField(upload_to="hotels", null=True)
 
     def __str__(self) -> str: #return a string representation of the amenity name.
         return self.name
